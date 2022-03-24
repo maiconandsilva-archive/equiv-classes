de91605cb9925f70f0fc20eeac11d1d936f658df
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2022-03-23 23:07:11 -0300

    Initial commit :rocket:

---
+++ src/main/java/io/github/maiconandsilva/equivclasses/EquivclassesApplication.java
@@ -0,0 +1,13 @@
+package io.github.maiconandsilva.equivclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquivclassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquivclassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/maiconandsilva/equivclasses/EquivclassesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconandsilva.equivclasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquivclassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
