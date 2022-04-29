b10ec282244062e0ef08941d3d3c9ffac9836740
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2022-04-28 22:49:10 -0300

    Commit missing class file View

---
+++ src/main/java/io/github/maiconandsilva/equivclasses/utils/View.java
@@ -0,0 +1,7 @@
+package io.github.maiconandsilva.equivclasses.utils;
+
+public interface View {
+    interface Minimal {}
+    interface Short extends Minimal {}
+    interface Extended extends Short {}
+}
