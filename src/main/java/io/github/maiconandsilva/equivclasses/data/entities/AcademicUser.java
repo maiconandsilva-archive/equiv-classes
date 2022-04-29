de896286513af13e5d057b151221354e990fa986
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2022-04-29 18:24:57 -0300

    Fix wrong return values AcademicUser

--- src/main/java/io/github/maiconandsilva/equivclasses/data/entities/AcademicUser.java
+++ src/main/java/io/github/maiconandsilva/equivclasses/data/entities/AcademicUser.java
@@ -52,16 +52,16 @@ public class AcademicUser extends BaseEntity<UUID> implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 }
