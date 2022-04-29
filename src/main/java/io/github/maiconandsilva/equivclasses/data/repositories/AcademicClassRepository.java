1678a6dcf6dda10c7bb4b1ffa925d51326013656
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2022-04-28 22:46:25 -0300

    Add queries for searching academic classes

--- src/main/java/io/github/maiconandsilva/equivclasses/data/repositories/AcademicClassRepository.java
+++ src/main/java/io/github/maiconandsilva/equivclasses/data/repositories/AcademicClassRepository.java
@@ -2,11 +2,25 @@ package io.github.maiconandsilva.equivclasses.data.repositories;
 
 import io.github.maiconandsilva.equivclasses.data.entities.AcademicClass;
 import io.github.maiconandsilva.equivclasses.data.entities.Course;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
 public interface AcademicClassRepository extends CrudRepository<AcademicClass, Long> {
+    @Query("from AcademicClass ac"
+            + " where lower(ac.name) like concat('%', lower(?1), '%')"
+            + " or lower(ac.code) like concat('%', lower(?1), '%')"
+            + " or lower(ac.course.name) like concat('%', lower(?1), '%')"
+            + " or lower(ac.course.code) like concat('%', lower(?1), '%')"
+    )
+    Set<AcademicClass> search(String search);
+
+    @Query("from AcademicClass ac where ac.equivalentClass.id = ("
+         + "select ac1.equivalentClass.id from AcademicClass ac1 where ac1.id = ?1)"
+    )
+    Set<AcademicClass> findAllEquivalentsByAcademicClassId(Long academicClassId);
+
     Set<AcademicClass> findAllByNameIgnoreCaseContaining(String name);
     Set<AcademicClass> findAllByCodeIgnoreCaseContaining(String code);
     Set<AcademicClass> findAllByCourse(Course course);
