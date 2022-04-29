445b97f9e136126b8cce4e594ea851b3a39252e8
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2022-04-28 22:45:48 -0300

    Add @JsonView annotations to id in BaseEntity

--- src/main/java/io/github/maiconandsilva/equivclasses/data/entities/BaseEntity.java
+++ src/main/java/io/github/maiconandsilva/equivclasses/data/entities/BaseEntity.java
@@ -1,10 +1,22 @@
 package io.github.maiconandsilva.equivclasses.data.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.github.maiconandsilva.equivclasses.utils.View;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import java.io.Serializable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class BaseEntity<ID extends Serializable> extends AbstractPersistable<ID> {
+    @Override
+    @JsonProperty
+    @JsonView(View.Minimal.class)
+    public ID getId() {
+        return super.getId();
+    }
+
     @Override
     public int hashCode() {
         return getClass().hashCode();
